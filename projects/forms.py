2217e9c47fe467410a55bbc78830e5151fafe957
dawn-wei <dawn_wei@outlook.com>
2019-04-16 14:50:26 +0800

    commit file

---
+++ projects/forms.py
@@ -0,0 +1,8 @@
+#coding:utf-8
+from django import forms
+from .models import Project
+
+class ProjectForm(forms.ModelForm):
+	class Meta:
+		model = Project
+		fields = ('project_name', 'project_location', 'project_time1', 'project_time2', 'project_map',)
